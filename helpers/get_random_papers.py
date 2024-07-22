04ac0a2ca22e64cbd8617877d5033196005a07e0
AA82758 <vasile_ciorna@goodyear.com>
2024-07-22 11:19:35 +0200

    random sampler paths updated

--- helpers/get_random_papers.py
+++ helpers/get_random_papers.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 import random
+from Levenshtein import distance as lev
 
 # the year when this code was generated
 random.seed(2024)
 
-def get_10_percent_papers(df:pd.DataFrame, scented_widgets: str = "FALSE") -> None:
+def get_randomn_papers_for_text_llms(df:pd.DataFrame, scented_widgets: str = "FALSE") -> None:
     subsetted_df = df[(df["scented_widgets_gemini_corrected"] == scented_widgets) & (df["scented_widgets_corrected"] == scented_widgets)]
     subsetted_df.reset_index(inplace=True, drop=True)
     number_of_papers = int(np.round(len(subsetted_df)*0.1))
@@ -16,10 +17,26 @@ def get_10_percent_papers(df:pd.DataFrame, scented_widgets: str = "FALSE") -> No
     print(resubset[["index_gemini", "Title"]])
 
 
+def get_randomn_papers_for_image_parsing_ai(df:pd.DataFrame, scented_widgets: str = "FALSE") -> None:
+    number_of_random_papers = 20
+    subsetted_df = df[df["scented_widgets_gemini_corrected"] == scented_widgets]
+    subsetted_df.reset_index(inplace=True, drop=True)
+    random_integers = [random.randint(0, len(subsetted_df) - 1) for _ in range(number_of_random_papers)]
+    resubset = subsetted_df.iloc[random_integers]
+    resubset.sort_values(by="index_gemini", inplace=True)
+    print(resubset[["index_gemini", "doi_gemini"]])
+
+
+
 def main():
-    df = pd.read_csv('../records_complete_snowballing_chatgpt_and_gemini.csv')
-    get_10_percent_papers(df)
-    get_10_percent_papers(df, "TRUE")
+    # that's for the text parser
+    df = pd.read_csv('../data/records_complete_snowballing_chatgpt_and_gemini.csv')
+    get_randomn_papers_for_text_llms(df)
+    get_randomn_papers_for_text_llms(df, "TRUE")
+    # that's for the image parser
+    df_image =  pd.read_csv('../data/vis30k_gemini_unique_answers.csv')
+    get_randomn_papers_for_image_parsing_ai(df_image)
+    get_randomn_papers_for_image_parsing_ai(df_image, "TRUE")
 
 
 
